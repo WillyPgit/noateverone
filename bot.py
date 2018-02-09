07f19fa61987c0334ba7147aebd516d3c0d8acc4
unknown <pearcewilliam222@gmail.com>
2018-02-09 19:55:47 -0400

    added preferences

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import discord
 import asyncio
-import random
-import pickle
-import os
+import json
 from tendo import singleton
 from profanities import profs
 from token1 import tok
+
 
 me = singleton.SingleInstance()
 client = discord.Client()
@@ -18,17 +17,104 @@ async def on_ready(): #prints log in info
 
 @client.event
 async def on_message(message):
+
+
+        if message.content.startswith("::enable everyone"):
+
+                data = json.load(open("prefs.json"))
+                value = data[str(message.server)]["everyone_disabled"]
+                data[str(message.server)]["everyone_disabled"] = True
+                with open("prefs.json", "w") as output:
+                        json.dump(data, output)
+
+                if value == True:
+                        mymessage = await client.send_message(message.channel, "```the use of @everyone is already forbidden in %s```" % (str(message.server)))
+                        await client.delete_message(message) #deletes command
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes confirmation message
+                else:
+                        mymessage = await client.send_message(message.channel, "```the use of @everyone is now forbidden in %s```" % (str(message.server))) #sends confirmation message
+                        await client.delete_message(message) #deletes command
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes confirmation message
+
+        if message.content.startswith("::disable everyone"):
+
+                data = json.load(open("prefs.json"))
+                value = data[str(message.server)]["everyone_disabled"]
+                data[str(message.server)]["everyone_disabled"] = False
+                with open("prefs.json", "w") as output:
+                        json.dump(data, output)
+
+                if value == False:
+                        mymessage = await client.send_message(message.channel, "```the use of @everyone is already permitted in %s```" % (str(message.server)))
+                        await client.delete_message(message) #deletes command
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes confirmation message
+                else:
+                        mymessage = await client.send_message(message.channel, "```the use of @everyone is now permitted in %s```" % (str(message.server))) #sends confirmation message
+                        await client.delete_message(message) #deletes command
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes confirmation message
+
         if message.mention_everyone: #deletes any message that contains the string @everyone
-                mymessage = await client.send_message(message.channel, "```@everyone is disabled on this server```")
-                await client.delete_message(message)
-                await asyncio.sleep(5)
-                await client.delete_message(mymessage)
+                
+                data = json.load(open("prefs.json"))
+                if data[str(message.server)]["everyone_disabled"] == True:
+
+                        mymessage = await client.send_message(message.channel, "```@everyone is disabled on this server```") #sends warning message
+                        await client.delete_message(message) #deletes the message containing @everyone
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes the warning message after 5 seconds
+               
+
+        if message.content.startswith("::enable filter"):
+
+                data = json.load(open("prefs.json"))
+                value = data[str(message.server)]["prof_disabled"]
+                data[str(message.server)]["prof_disabled"] = True
+                with open("prefs.json", "w") as output:
+                        json.dump(data, output)
+
+                if value == True:
+                        mymessage = await client.send_message(message.channel, "```the use of profanity is already forbidden in %s```" % (str(message.server)))
+                        await client.delete_message(message) #deletes command
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes confirmation message
+                else:
+                        mymessage = await client.send_message(message.channel, "```the use of profanity is now forbidden in %s```" % (str(message.server))) #sends confirmation message
+                        await client.delete_message(message) #deletes command
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes confirmation message
+
+        if message.content.startswith("::disable filter"):
+
+                data = json.load(open("prefs.json"))
+                value = data[str(message.server)]["prof_disabled"]
+                data[str(message.server)]["prof_disabled"] = False
+                with open("prefs.json", "w") as output:
+                        json.dump(data, output)
+
+                if value == False:
+                        mymessage = await client.send_message(message.channel, "```the use of profanity is already permitted in %s```" % (str(message.server)))
+                        await client.delete_message(message) #deletes command
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes confirmation message
+                else:
+                        mymessage = await client.send_message(message.channel, "```the use of profanity is now permitted in %s```" % (str(message.server))) #sends confirmation message
+                        await client.delete_message(message) #deletes command
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes confirmation message
 
         stripped = "".join([x for x in message.content if ord(x) < 128]).lower()
         if any([word in stripped for word in profs]): #deletes any message that contains profanity
-                mymessage = await client.send_message(message.channel, "```profanities are not allowed on this server```")
-                await client.delete_message(message)
-                await asyncio.sleep(5)
-                await client.delete_message(mymessage)
+                
+                data = json.load(open("prefs.json"))
+                if data[str(message.server)]["prof_disabled"] == True:
+
+                        mymessage = await client.send_message(message.channel, "```profanities are not allowed on this server```") #sends warning message
+                        await client.delete_message(message) #deletes the message containing profanity
+                        await asyncio.sleep(5)
+                        await client.delete_message(mymessage) #deletes the warning message after 5 seconds
 
 client.run(tok)
